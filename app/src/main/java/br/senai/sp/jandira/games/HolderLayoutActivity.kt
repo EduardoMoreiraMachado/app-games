0e07575fb882516f63f51b21137642195856d0a7
EddieTheBill <dudumm1234@gmail.com>
2022-10-21 11:23:51 -0300

    first commit

---
+++ app/src/main/java/br/senai/sp/jandira/games/HolderLayoutActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.games
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HolderLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.holder_layout)
+    }
+}
---
+++ app/src/main/java/br/senai/sp/jandira/games/SignUpActivity.kt
@@ -0,0 +1,47 @@
+package br.senai.sp.jandira.games
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.menu_save) {
+            Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show()
+
+            return true
+
+        } else if (item.itemId == R.id.menu_settings) {
+
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+
+            return true
+
+        } else {
+
+            Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show()
+
+            return true
+
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_new_user, menu)
+
+        return true
+
+    }
+
+}
